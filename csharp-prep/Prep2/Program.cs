49fa49d053fdb2746acfae7d84bedb9ce95a6055
Myamikki <myamikayla8@gmail.com>
2024-11-05 20:41:25 +0200

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask the user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        // Create a variable to store the letter grade
+        string letter = "";
+        string sign = ""; // To store the "+" or "-" sign
+
+        // Determine the letter grade
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Only add "+" or "-" for B, C, and D grades (A+ does not exist, F+ and F- do not exist)
+        if (letter != "A" && letter != "F")
+        {
+            int lastDigit = gradePercentage % 10; // Get the last digit of the percentage
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Print the letter grade with sign (if applicable)
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        // Determine if the user passed the course
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Keep trying and you'll do better next time.");
+        }
     }
 }
