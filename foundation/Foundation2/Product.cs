2920d504b2c6892368e8156297f45d8dec7de456
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-12-07 19:17:02 +0200

    Create Product.cs

---
+++ foundation/Foundation2/Product.cs
@@ -0,0 +1,27 @@
+/*This class stores details of a product for example the name, id
+price and quantity */
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public string Name { get => _name; set => _name = value; }
+    public string ProductId { get => _productId; set => _productId = value; }
+    public double Price { get => _price; set => _price = value; }
+    public int Quantity { get => _quantity; set => _quantity = value; }
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public double CalculateTotalCost()
+    {
+        return _price * _quantity;
+    }
+}
