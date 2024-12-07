91759d99a9c1d3e950f9822a591c0c604aa8c01a
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-12-07 19:10:19 +0200

    Update Program.cs

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        //Order 1
+        Address address1 = new Address("1528 Bezuidenhout St", "Piet Retief", "Mpumalanga", "South Africa");
+        Customer customer1 = new Customer("Janet Peters", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("TELEVISION ISTAR 55", "Smart S55GW01-Black", 1500.00, 1));
+        order1.AddProduct(new Product("Hair Dryer", "BHD510/00", 76.00, 3));
+
+        //Order 2
+        Address address2 = new Address("1631 Telford Ave", "Vryburg", "North West", "South Africa");
+        Customer customer2 = new Customer("Roberts Smith", address2);
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Samsung Yellow Toner", "Y504S", 100.00, 3));
+        order2.AddProduct(new Product("Pure Digital Sport Watch", "NFS7201-G", 20.00, 2));
+
+        //results for Order 1
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost():F2}\n");
+
+        //results for Order 2
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost():F2}\n");
     }
 }
