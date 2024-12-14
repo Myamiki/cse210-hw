ca64ebe78e1b3ff1fe08982254bffeeaac4fa83c
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-12-14 16:41:43 +0200

    Update Program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create a base "Assignment" object
+        Assignment FirstAssignment = new Assignment("Nicky Tanya", "Statistics");
+        Console.WriteLine(FirstAssignment.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment SecAssignment = new MathAssignment("Mikayla Sithole", "Equations", "9.0", "18-19");
+        Console.WriteLine(SecAssignment.GetSummary());
+        Console.WriteLine(SecAssignment.GetHomeworkList());
+
+        WritingAssignment ThirdAssignment = new WritingAssignment("Mya Sithole", "English", "Verbs");
+        Console.WriteLine(ThirdAssignment.GetSummary());
+        Console.WriteLine(ThirdAssignment.GetWritingInformation());
     }
 }
