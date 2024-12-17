469e7c94f63416913424dc5432fcfa8627078929
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-12-17 05:56:32 +0200

    Update Program.cs

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 12, 18), 40, 8.0),
+            new Cycling(new DateTime(2024, 12, 18), 30, 60.0),
+            new Swimming(new DateTime(2024, 12, 18), 25, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.ActivitySummary());
+        }
     }
 }
+/* 
+ This program defines a list of different types of activities Running, Cycling, Swimming and stores them in a list called activities.
+Each activity has a date, duration, and distance or time for swimming. 
+The program then loops through each activity and prints a summary of it using the ActivitySummary method.
+*/
