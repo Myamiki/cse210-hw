d049c70d84b53a9f9c7e516afafa3ddbd1fecdda
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-12-19 07:28:32 +0200

    Update Goal.cs

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -16,4 +16,7 @@ public abstract class Goal
     public abstract string GetDetailsString();
     public abstract string ToFileString();
 }
-
+//Creativity
+/*This file has the basic information that all types of goals share, like the goal's name, description, and points.
+It’s like a blueprint for goals, setting up what every goal should have.
+This shows abstraction, meaning we focus on what’s common to all goals without worrying about the details of each type of goal. */
